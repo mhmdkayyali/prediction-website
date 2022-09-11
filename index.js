4b5393894c6081f7ed75d8f67eaad052961665b2
mhmdkayyali <mohammad.a.kayyali@gmail.com>
2022-09-11 04:48:51 +0300

    fetching gender and age

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const submit = document.getElementById("submit");
 
 submit.addEventListener("click", predicting)
 
-function predicting (){
 fetch("https://dog.ceo/api/breeds/image/random")
     .then(Response => Response.json())
     .then(data => {
         if(data.message)
         dogid.innerHTML = `<img src="${data.message}"/>`
     })
+
+// console.log(name)
+
+function predicting(){
+    // https://api.genderize.io/?name=nour
+    let apiGender = "https://api.genderize.io/?name="
+    let name = document.getElementById("name").value
+    let genderApiKey = apiGender + name
+    console.log(genderApiKey)
+    fetch(genderApiKey)
+        .then(Response => Response.json())
+        .then(data => {
+            document.getElementById("gender").innerHTML = data.gender
+        })
+    
+    let apiAge = "https://api.agify.io/?name="
+    let name1 = document.getElementById("name").value
+    let ageApiKey = apiAge + name1
+    console.log(ageApiKey)
+    fetch(ageApiKey)
+        .then(Response => Response.json())
+        .then(data => {
+            document.getElementById("age").innerHTML = data.age
+        })
+
+
 }
